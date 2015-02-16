b7d12a1a92a51ad2cd2a9965b6462760700b15ce
yaroslav.n <yaroslav.sib@yandex.ru>
2015-02-16 20:20:35 +0600

    new

--- 2.1/model.php
+++ 2.1/model.php
@@ -1,25 +1,24 @@
 <?php
 
-
-
-
-$totalpage = (isset($_GET['total']) ? $_GET['total'] : 10);
-$currentpage = (isset($_GET['page']) ? $_GET['page'] : 1);
-$firstpage = 1;
-$lastpage = $totalpage;
-$loopcounter = ( ( ( $currentpage + 2 ) <= $lastpage ) ? ( $currentpage + 2 ) : $lastpage );
-$startCounter = ( ( ( $currentpage - 2 ) >= 3 ) ? ( $currentpage - 2 ) : 1 );
-
-if($totalpage > 1) {
-  $pagination = '<ul class="paginate" id="paginate">';
-  $pagination .= '<li><a href="http://localhost/Home2/2.1/model.php?page=1" class="paginate_click" id="1-page">First</a></li>';
-    for($i = $startCounter; $i <= $loopcounter; $i++) {
-      $pagination .= '<li><a href="http://localhost/Home2/2.1/model.php?page='.$i.'">'.$i.'</a></li>';
-    }
-  $pagination .= '<li><a href="http://localhost/Home2/2.1/model.php?page='.$totalpage.'" class="paginate_click" id="'.$totalpage.'-page">Last</a></li>';
-  $pagination .= '</ul>';
+function pagination($pageCount, $currentPage, $offset){
+	$totalpage = (isset($_GET['total']) ? $_GET['total']:$pageCount);
+	$currentpage = (isset($_GET['page']) ? $_GET['page'] : 1);
+	$firstpage = 1;
+	$lastpage = $pageCount;
+	$loopcounter = ( ( ( $currentPage + $offset ) <= $lastpage ) ? ( $currentPage + $offset ) : $lastpage );
+	$startcounter = ( ( ( $currentPage - $offset) >= 1+$offset ) ? ( $currentPage - $offset ) : 1 );
+	if($pageCount > 1) {
+		$pagination = '<ul class="paginate" id="paginate">';
+		$pagination .= '<li><a href="http://localhost/pagination/model.php?page=1" class="paginate_click" id="1-page">1</a></li>';
+			for($i = $startcounter; $i <= $loopcounter; $i++) {
+				$pagination .= '<li><a href="http://localhost/pagination/model.php?page='.$i.'">'.$i.'</a></li>';
+			}
+		$pagination .= '<li><a href="http://localhost/pagination/model.php?page='.$pageCount.'" class="paginate_click" id="'.$pageCount.'-page">Last</a></li>';
+		$pagination .= '</ul>';
+	}
+	echo $pagination;
 }
-
-echo $pagination;
+pagination(20,9,2);
 
 ?>
+
