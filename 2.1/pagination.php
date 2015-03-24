34c892bf7c11ab89e6bed4a917280997358c46a5
yaroslav.n <yaroslav.sib@yandex.ru>
2015-03-24 18:21:46 +0600

    final version

--- 2.1/pagination.php
+++ 2.1/pagination.php
@@ -1,11 +1,16 @@
-<?php
+ <?php
 
-function Paginator($pageCount, $currentPage, $offset) {
-    if ($currentPage > $pageCount or (!is_int($pageCount)) or (!is_int($currentPage)) or (!is_int($offset)) or $pageCount < 0 or $currentPage < 0 or $offset < 0) { //If variable is not a number or is a minus number - throw exception
-        echo "Error";
-    } else {
-        //It can be three possible cases: 1) $currentPage > $offset; 2) $currentPage < $offset; 3) $currentPage = $offset;
-        // 1) case;
+function paginator($pageCount, $currentPage, $offset) {
+    if ($currentPage > $pageCount) {
+        throw new \Exception('Текущая страница больше общего количества страниц');
+    }
+    elseif ((!is_int($pageCount)) or (!is_int($currentPage)) or (!is_int($offset))) {
+        throw new \Exception('Аргументы должны быть числами!');
+    }
+    elseif ($pageCount < 0 or $currentPage < 0 or $offset < 0) {
+        throw new \Exception('Аргументы должны быть больше нуля');
+    }
+    else {
         if ($currentPage > $offset) {
             if ($offset + 1 == $currentPage or $offset + 2 == $currentPage) {
                 for ($i = 1; $i < $currentPage; $i++) {
@@ -29,8 +34,7 @@ function Paginator($pageCount, $currentPage, $offset) {
                 }
             }
         }
-
-        //2) case
+//2) case
         elseif ($currentPage < $offset) {
             for ($i = 1; $i < $currentPage; $i++) {
                 echo " " . $i . " ";
@@ -47,26 +51,32 @@ function Paginator($pageCount, $currentPage, $offset) {
                 }
             }
         }
-
-        // 3) case;
-        elseif ($currentPage = $offset) {
-            if ($currentPage + $offset >= $pageCount) {
-                for ($i = 1; $i <= $pageCount; $i++) {
-                    echo " " . $i . " ";
-                }
+// 3) case;
+        elseif ($currentPage == $offset) {
+            if ($currentPage == 1) {
+                echo "<span style='font-size: 24px'>$currentPage</span>";
             } else {
                 for ($i = 1; $i < $currentPage; $i++) {
                     echo " " . $i . " ";
                 }
                 echo "<span style='font-size: 24px'>$currentPage</span>";
-                for ($i = $currentPage + 1; $i <= $currentPage + $offset; $i++) {
-                    echo " " . $i . " ";
-                }
                 if ($currentPage + $offset < $pageCount - 1) {
+                    for ($i = $currentPage + 1; $i <= $currentPage + $offset; $i++) {
+                        echo " " . $i . " ";
+                    }
                     echo "...";
+                    echo $pageCount;
+                } else {
+                    for ($i = $currentPage + 1; $i <= $pageCount; $i++) {
+                        echo " " . $i . " ";
+                    }
                 }
-                echo " " . $pageCount;
             }
         }
     }
+}
+try {
+    paginator(20, 13, 13);
+} catch (\Eception $e) {
+    echo $e->getMessage();
 }
